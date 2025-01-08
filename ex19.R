9b92b62744c37ceae65cf5e008321d280282b311
User <user@Eozbs-MacBook-Pro.local>
2025-01-08 16:30:36 +0100

    fixed ex 19 into line

--- ex19.R
+++ ex19.R
@@ -6,16 +6,16 @@
 # - `ChiffreAffaires` : `1000, 1200, 1500, 1700`
 # - Tracez un graphique linéaire où l'axe `x` représente les mois et l'axe `y` représente le chiffre d'affaires.
 # - Ajoutez des titres au graphique et aux axes.
+#install.packages("ggplot2")
 library(ggplot2)
 ventes <- data.frame (
   Mois = c("Janvier", "Fevrier", "Mars", "Avril"),
   ChiffreAffaires = c(1000, 1200, 1500, 1700)
 )
-ggplot(data = ventes, aes(x = Mois, y = ChiffreAffaires, fill =Mois)) +
-  geom_bar(stat = "identity") +
+ggplot(data=ventes, aes(x = Mois, y = ChiffreAffaires, group =1)) +
+  geom_line() +
   labs(
     title = "Totaux des chiffre d'affaires par mois",
     x = "Mois",
     y = "Chiffre d'affaires"
-  ) 
-
+  )
