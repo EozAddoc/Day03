b51e61a37dadaab2c66ddeaea04d041073f3b7e0
User <user@Eozbs-MacBook-Pro.local>
2025-01-08 16:39:39 +0100

    fixed exercise 20 forgot to use sample

--- ex20.R
+++ ex20.R
@@ -3,8 +3,8 @@
 #   - Simulez 100 lancers de dés avec la fonction `sample()`, en choisissant un nombre entre 1 et 6 pour chaque lancer.
 # - Comptez la fréquence de chaque résultat (1 à 6) avec la fonction `table()`.
 # - Affichez ces fréquences sous forme d’un barplot avec `barplot()`.
-set.seed()
-des = round(runif(100, min=1, max=6))
-freq = table(round(runif(100, min=1, max=6)))
+
+des = sample(1:6,100, replace =TRUE)
+freq = table(round(des))
 barplot(freq,des)
 
